56eb5f82d6f8a15d6febfebf120fc1f94352423a
Yash <yashpatelv21@gmail.com>
2023-11-03 00:41:41 +0530

    date: 03/11/2023

--- app/src/main/java/com/example/mad_practical_7_21012021098/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_7_21012021098/MainActivity.kt
@@ -1,24 +1,32 @@
 package com.example.mad_practical_7_21012021098
 
+import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.widget.MediaController
 import android.widget.VideoView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val myVideoview: VideoView = findViewById(R.id.videoView)
+        val myVideoView: VideoView = findViewById(R.id.myvideo)
         val mediaController = MediaController(this)
-        val uri: Uri = Uri.parse("android resource://" +packageName + "/" + R.raw.thestoryoflight)
-        myVideoview.setMediaController(mediaController)
-        mediaController.setAnchorView(myVideoview)
-        myVideoview.setVideoURI(uri)
-        myVideoview.requestFocus()
-        myVideoview.start()
+        val uri: Uri = Uri.parse("android.resource://" + packageName + "/" + R.raw.thestoryoflight)
+        mediaController.setAnchorView(myVideoView)
+        myVideoView.setMediaController(mediaController)
+        myVideoView.setVideoURI(uri)
+        myVideoView.requestFocus()
+        myVideoView.start()
+
+        val switchButton: FloatingActionButton = findViewById(R.id.floatingActionButton)
+
+        switchButton.setOnClickListener() {
+            Intent(this@MainActivity, YoutubeActivity::class.java).also { startActivity(it) }
+        }
     }
 }
--- app/src/main/java/com/example/mad_practical_7_21012021098/YoutubeActivity.kt
+++ app/src/main/java/com/example/mad_practical_7_21012021098/YoutubeActivity.kt
@@ -1,22 +1,41 @@
 package com.example.mad_practical_7_21012021098
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebSettings
 import android.webkit.WebView
 import com.example.mad_practical_7_21012021098.R.id.youtube_webview
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.androidgamesdk.gametextinput.Settings
 
 class YoutubeActivity : AppCompatActivity() {
+
+    private fun initYoutubePlayer() {
+        val webView = findViewById<WebView>(R.id.youtube_webview)
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.loadWithOverviewMode = true
+        webSettings.useWideViewPort = true
+
+        // Replace this with the standard YouTube URL
+        val youtubeUrl = "https://www.youtube.com/watch?v=L5Qz0_45vOI"
+
+        // Load the YouTube URL into the WebView
+        webView.loadUrl(youtubeUrl)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_youtube)
 
-        val youtubeId = "fJn9B64Znrk"
-        val youtubeWebView: WebView=findViewById(R.id.youtube_webview)
-        val webSettings:WebSettings = youtubeWebView.settings
-        webSettings.javaScriptEnabled = true
-        webSettings.loadWithOverviewMode = true
-        webSettings.useWideViewPort = true
-        youtubeWebView.loadUrl("https://www.youtube.com/embed/$youtubeId")
+        // Initialize the WebView to display the YouTube video
+        initYoutubePlayer()
+
+        val ytWebView: FloatingActionButton = findViewById(R.id.floatingActionButton2)
+
+        ytWebView.setOnClickListener() {
+            Intent(this@YoutubeActivity, MainActivity::class.java).also { startActivity(it) }
+        }
     }
 }
